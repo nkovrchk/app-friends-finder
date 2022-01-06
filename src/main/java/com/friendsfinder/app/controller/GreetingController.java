0590cc6abd307fc08082af4ffa87da541d9cebae
nkovrchk <nkovrchk@gmail.com>
2022-01-06 13:48:53 +0300

    Initial commit

---
+++ src/main/java/com/friendsfinder/app/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.friendsfinder.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController{
+    private static final String template = "Hello, %s!";
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "friend") String name){
+        return String.format(template, name);
+    }
+}
