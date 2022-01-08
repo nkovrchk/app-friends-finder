4b2809c130f277cbed254019cb72593b927c1f5e
nkovrchk <nkovrchk@gmail.com>
2022-01-08 11:22:09 +0300

    added Dockerfile

--- src/main/java/com/friendsfinder/app/controller/GreetingController.java
+++ src/main/java/com/friendsfinder/app/controller/GreetingController.java
@@ -1,10 +1,12 @@
 package com.friendsfinder.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class GreetingController{
     private static final String template = "Hello, %s!";
 
