935654a7c676338ab1dd412345b80a82eb5a635d
nkovrchk <nkovrchk@gmail.com>
2022-01-09 15:44:07 +0300

    added test api call

--- src/main/java/com/friendsfinder/app/controller/GreetingController.java
+++ src/main/java/com/friendsfinder/app/controller/GreetingController.java
@@ -1,5 +1,9 @@
 package com.friendsfinder.app.controller;
 
+import com.friendsfinder.app.service.VKApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class GreetingController{
+    @Autowired
+    private VKApiService vkApiService;
+
     private static final String template = "Hello, %s!";
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name", defaultValue = "friend") String name){
         return String.format(template, name);
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<String> getTodos(){
+        return new ResponseEntity<>(vkApiService.getUserInfo(), HttpStatus.OK);
     }
 }
